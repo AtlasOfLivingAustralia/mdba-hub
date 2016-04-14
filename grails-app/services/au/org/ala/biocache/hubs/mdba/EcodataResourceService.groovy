cd68f8c0c3c5f1db11d433fdb9801dc07fa592a8
chrisala <chris.godwin.ala@gmail.com>
2016-04-14 14:33:03 +1000

    Changed ecodata paths to include /ws instead of relying on it being in the config.

--- grails-app/services/au/org/ala/biocache/hubs/mdba/EcodataResourceService.groovy
+++ grails-app/services/au/org/ala/biocache/hubs/mdba/EcodataResourceService.groovy
@@ -1,11 +1,9 @@
 package au.org.ala.biocache.hubs.mdba
 
 import au.org.ala.ws.service.WebService
-import grails.converters.JSON
 import org.apache.http.entity.ContentType
 import org.codehaus.groovy.grails.commons.GrailsApplication
 import org.springframework.web.multipart.MultipartFile
-
 
 class EcodataResourceService {
 
@@ -13,29 +11,29 @@ class EcodataResourceService {
     GrailsApplication grailsApplication
 
     def get(String id) {
-        def url = "${grailsApplication.config.ecodata.baseURL}/document/${id}"
+        def url = "${grailsApplication.config.ecodata.baseURL}/ws/document/${id}"
         return webService.get(url)
     }
 
     def delete(String id) {
-        def url = "${grailsApplication.config.ecodata.baseURL}/document/${id}"
+        def url = "${grailsApplication.config.ecodata.baseURL}/ws/document/${id}"
         return webService.delete(url, [:], ContentType.TEXT_PLAIN)
     }
 
     def updateDocument(doc) {
-        def url = "${grailsApplication.config.ecodata.baseURL}/document/${doc.documentId?:''}"
+        def url = "${grailsApplication.config.ecodata.baseURL}/ws/document/${doc.documentId?:''}"
 
         return webService.post(url, doc)
     }
 
     def updateDocument(Map doc, MultipartFile file) {
-        def url = grailsApplication.config.ecodata.baseURL + "/document/${doc.documentId?:''}"
+        def url = grailsApplication.config.ecodata.baseURL + "/ws/document/${doc.documentId?:''}"
         def result = webService.postMultipart(url, [document:doc], [:], [file])
         return result
     }
 
     def Map search(Map params) {
-        def url = "${grailsApplication.config.ecodata.baseURL}/document/search"
+        def url = "${grailsApplication.config.ecodata.baseURL}/ws/document/search"
         def resp = webService.post(url, params)
         if (resp && !resp.error) {
             return resp.resp
