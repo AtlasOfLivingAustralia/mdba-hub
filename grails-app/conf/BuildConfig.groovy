936bfffa634515a3c740005d2603692482756cd2
Peter Ansell <p_ansell@yahoo.com>
2017-06-23 10:14:06 +1000

    Revert "Bump to Grails-2.5.6"
    This reverts commit 36fa0c49b1ba795acaddb18d1c7271ddefb9eb47.
    
    MDBA is heavily customised and not compatible with the current biocache-hubs. Needs a dedicated sprint to work through the compatibility issues between biocache-hubs:0.75 and biocache-hubs:1.3.12
    
    Signed-off-by: Peter Ansell <p_ansell@yahoo.com>

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -54,10 +54,10 @@ grails.project.dependency.resolution = {
         compile ":document-preview-plugin:0.2"
         compile ":ala-ws-plugin:1.0"
         runtime ':font-awesome-resources:4.3.0.1'
-        runtime (":biocache-hubs:1.3.12") {
+        runtime (":biocache-hubs:0.75") {
             excludes "release", "jquery"
         }
-        runtime (':collectory-hub:1.1.2') {
+        runtime (':collectory-hub:1.1.1') {
             excludes 'jquery'
         }
 
